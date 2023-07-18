0a8491b507d04590a54be60362c947049defde72
evy1-ua <evy1@alu.ua.es>
2023-07-18 20:07:17 +0200

    ahora

--- src/App.js
+++ src/App.js
@@ -25,37 +25,37 @@ function App() {
           <Route path="/" element={<Index />} />
           <Route path="/index" element={<Index />} />
           <Route path="/login" element={<Login />} />
-          <Route path="/dashboard" element={<Dashboard />}/>
+          <Route path="/dashboard" element={<DashboardRoute />}/>
         </Routes>
       </Router>
     </div>
   );
 }
-// function DashboardRoute() {
-//   const [isAuthenticated, setIsAuthenticated] = useState(false);
-//   const [user, setUser] = useState(null);
-//   useEffect(() => {
-//     fetch('/dashboard')
-//       .then(response => response.json())
-//       .then(data => {
-//         if(data.user) {
-//           setIsAuthenticated(true);
-//           setUser(data.user);
-//         }else{
-//           setIsAuthenticated(false);
-//         }        
-//       })    
-//       .catch(error => {
-//         console.error('Error al obtener el usuario:', error);
-//         setIsAuthenticated(false);
-//         // Manejar el error, por ejemplo, redirigir a la página de inicio de sesión
-//       });
-//   }, []);
+function DashboardRoute() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [user, setUser] = useState(null);
+  useEffect(() => {
+    fetch('/dashboard')
+      .then(response => response.json())
+      .then(data => {
+        if(data.user) {
+          setIsAuthenticated(true);
+          setUser(data.user);
+        }else{
+          setIsAuthenticated(false);
+        }        
+      })    
+      .catch(error => {
+        console.error('Error al obtener el usuario:', error);
+        setIsAuthenticated(false);
+        // Manejar el error, por ejemplo, redirigir a la página de inicio de sesión
+      });
+  }, []);
 
-//   if(!isAuthenticated){
-//     return <Navigate to="/" />;
-//   }
-//   return <Dashboard user={user} />
-// }
+  if(!isAuthenticated){
+    return <Navigate to="/" />;
+  }
+  return <Dashboard user={user} />
+}
 
 export default App;
