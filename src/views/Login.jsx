388ae2c50dd6bd7427c3f9ea5c4ae1f90d0e180d
evy1-ua <evy1@alu.ua.es>
2023-08-10 03:59:55 +0200

    h

--- src/views/Login.jsx
+++ src/views/Login.jsx
@@ -1,11 +1,10 @@
 import React, { useState } from 'react';
-import {useNavigate} from 'react-router-dom';
 import axios from 'axios';
 
 function Login() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const navigate = useNavigate();
+
 
   const handleEmailChange = (e) => {
     setEmail(e.target.value);
